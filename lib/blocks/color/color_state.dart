766a2199b8cba57c7cd60c805e088f44eff59671
Samet Tonbul <stsamettonbul@gmail.com>
2022-10-20 23:20:29 +0300

    bloc_state_with_change_color

---
+++ lib/blocks/color/color_state.dart
@@ -0,0 +1,23 @@
+part of 'color_bloc.dart';
+
+class ColorState extends Equatable {
+  final Color color;
+
+  const ColorState({required this.color});
+
+  factory ColorState.initial() => const ColorState(color: Colors.pink);
+
+  @override
+  List<Object> get props => [color];
+
+  @override
+  bool get stringify => true;
+
+  ColorState copyWith({
+    Color? color,
+  }) {
+    return ColorState(
+      color: color ?? this.color,
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'blocks/color/color_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => ColorBloc(),
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: BlocBuilder<ColorBloc, ColorState>(
+            builder: (context, state) {
+              return Container(
+                color: state.color,
+                child: FloatingActionButton(onPressed: () {
+                  context.read<ColorBloc>().add(ChangeColorEvent());
+                }),
+              );
+            },
+          )),
+    );
+  }
+}
